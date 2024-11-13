0519aa2233229b2771697b3ec745d298c293e52a
Pola Waffo Phares Valentin <99049857+651118070@users.noreply.github.com>
2024-11-13 12:48:52 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,10 @@ const corpsOtions = {
   optionsSucessStatus: 200,
   credentials: true,
 };
-app.use(cors());
+app.use(cors({
+  origin: 'https://mboashopsite.onrender.com', 
+  credentials: true,
+}));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
