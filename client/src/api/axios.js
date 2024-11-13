62909c40514bb39e6ff5c32e6499b257a08e315c
Pola Waffo Phares Valentin <99049857+651118070@users.noreply.github.com>
2024-11-13 08:59:37 +0100

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Base_URL = "http://localhost:3000";
+const Base_URL = "https://mboashop.onrender.com";
 
 const axiosInstance = axios.create({
   baseURL: Base_URL,
