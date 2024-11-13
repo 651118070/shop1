0a65d6fe8ebfb00c83a983b2eba82b1b7275f1ab
Pola Waffo Phares Valentin <99049857+651118070@users.noreply.github.com>
2024-11-13 09:09:39 +0100

    Update allowedOrigins.js

--- server/utils/allowedOrigins.js
+++ server/utils/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
     "http://127.0.0.1:5500",
     "http://localhost:5173",
     "http://localhost:3000",
+    "https://mboashopsite.onrender.com"
   ];
   
   export default allowedOrigins;
