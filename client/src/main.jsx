3bf927ae261b3c44c79fc889a3c7ce7bc4f7c515
651118070 <valentinpola7@gmail.com>
2024-11-21 21:19:20 +0100

    ready for production

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,10 +6,20 @@ import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { Toaster } from "./components/ui/toaster.jsx";
 
+import { PostHogProvider} from 'posthog-js/react'
+
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+}
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
+    <PostHogProvider 
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
       <App />
+    </PostHogProvider>
       <Toaster />
     </Provider>
   </BrowserRouter>
