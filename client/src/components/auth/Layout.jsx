7154a5ef14ce71bcaaf347ac0ff182312f652ac0
651118070 <valentinpola7@gmail.com>
2025-07-24 05:25:42 +0100

    change title shoppy site

--- client/src/components/auth/Layout.jsx
+++ client/src/components/auth/Layout.jsx
@@ -7,7 +7,7 @@ export default function AuthLayout() {
       <div className="hidden lg:flex items-center justify-center pr-2 bg-gray-900 dark:bg-blue-900/15 ">
         <div className=" max-w-md space-y-6 text-center text-primary-foreground ">
           <h1 className="text-4xl font-extrabold tracking-tight pl-2 text-white ">
-            Welcome to Shoppy Site.
+            Welcome to Mboa Shop.
           </h1>
         </div>
       </div>
