df04a9e6228afd9df9058478016636f8a880c4b1
651118070 <valentinpola7@gmail.com>
2025-07-24 05:45:55 +0100

    ok

--- client/src/components/shopping/Header.jsx
+++ client/src/components/shopping/Header.jsx
@@ -140,7 +140,7 @@ export default function ShoppingHeader() {
       <div className="flex h-16 items-center justify-between px-4 md:px-6">
         <Link to="/shop/home" className="flex items-center gap-2">
           <HousePlug className="h-6 w-6" />
-          <span className="font-bold">Mboa Boutique</span>
+          <span className="font-bold">Mboa Shop</span>
         </Link>
         <Sheet>
           <SheetTrigger asChild>
